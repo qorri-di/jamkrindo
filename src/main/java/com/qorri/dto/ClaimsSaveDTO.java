5d50a2039f53f4656d2fed0e7cd8dc72d967c39a
MrR007 <qorri24j@gmail.com>
2024-10-08 20:05:17 +0700

    add ClaimsSaveDTO

--- src/main/java/com/qorri/dto/ClaimsSaveDTO.java
+++ src/main/java/com/qorri/dto/ClaimsSaveDTO.java
@@ -1,4 +1,16 @@
 package com.qorri.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClaimsSaveDTO {
+    private String id;
+    private Integer periode;
+    private String lob;
+    private String penyebab_claim;
+    private String nasabah;
+    private String nilai_claim;
 }
