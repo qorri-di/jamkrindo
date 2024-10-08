a7e5fcb0c52526f2d4e3cc9b512967abd37c5629
MrR007 <qorri24j@gmail.com>
2024-10-08 20:06:58 +0700

    add ClaimsReadDTO

--- src/main/java/com/qorri/dto/ClaimsReadDTO.java
+++ src/main/java/com/qorri/dto/ClaimsReadDTO.java
@@ -1,4 +1,13 @@
 package com.qorri.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClaimsReadDTO {
+    private Integer limit;
+    private Integer offset;
+    private Integer page;
 }
